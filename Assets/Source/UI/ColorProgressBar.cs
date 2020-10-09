d690adcf202cad949dfd45c1354e815a2f2a536a
José Villegas <villegasjose.gg@gmail.com>
2020-10-09 18:42:33 -0300

    Working on progress bar logic and UI/UX

---
+++ Assets/Source/UI/ColorProgressBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorProgressBar : MonoBehaviour
+{
+    [SerializeField] private Graphic[] _graphicsToSetColor;
+
+    public void Setup(Color color, IGameConfiguration configuration)
+    {
+        foreach (var image in _graphicsToSetColor)
+        {
+            image.color = color;
+        }
+    }
+}
---
+++ Assets/Source/UI/ColorProgressBarInstancer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ColorProgressBarInstancer : MonoBehaviour
+{
+    [SerializeField] private ColorProgressBar _colorProgressBarPrefab;
+    private IGameConfiguration _configuration;
+
+    private void Start()
+    {
+        _configuration = Contexts.sharedInstance.configuration.gameConfiguration.value;
+        InstanceProgressBars();
+    }
+
+    private void InstanceProgressBars()
+    {
+        for (int i = 0; i < _configuration.BalloonColors.Length; i++)
+        {
+            var progress = Instantiate(_colorProgressBarPrefab, transform);
+            progress.Setup(_configuration.BalloonColors[i], _configuration);
+        }
+    }
+}
