05a48c1536f04eaf537e752150f3b01a2e2c7609
Jose Villegas <villegasjose.gg@gmail.com>
2020-11-17 14:53:32 -0300

    adding animator trigger after level up

---
+++ Assets/Source/UI/LevelUpPopUp.cs
@@ -0,0 +1,21 @@
+﻿using Entitas;
+using UnityEngine;
+
+public class LevelUpPopUp : EntityLinkerController, IAnyGameLevelUpListener
+{
+    [SerializeField] private Animator _animator;
+
+    protected override void DefineEntity(IEntity e)
+    {
+        base.DefineEntity(e);
+
+        var gameEntity = e as GameEntity;
+        
+        gameEntity.AddAnyGameLevelUpListener(this);
+    }
+
+    public void OnAnyGameLevelUp(GameEntity entity)
+    {
+        _animator.SetTrigger("Appear");
+    }
+}
