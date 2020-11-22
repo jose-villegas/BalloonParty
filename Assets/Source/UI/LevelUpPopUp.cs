c422d32a49fb4000d00d7d173a125a6b041cf8af
Jose Villegas <villegasjose.gg@gmail.com>
2020-11-22 13:18:24 -0300

    fine tuning level up animation popup

--- Assets/Source/UI/LevelUpPopUp.cs
+++ Assets/Source/UI/LevelUpPopUp.cs
@@ -1,4 +1,5 @@
-﻿using Entitas;
+﻿using System.Collections;
+using Entitas;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,10 @@ public class LevelUpPopUp : EntityLinkerController, IAnyGameLevelUpListener
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private Text _levelLabel;
+    [SerializeField] private Image _levelGlowFill;
+    [SerializeField] private ParticleSystem _levelGlowFillParticleSystem;
+    [Header("Animation")] [SerializeField] private float _fillAnimationDelay;
+    [SerializeField] private float _playParticlesDelay;
 
     protected override void DefineEntity(IEntity e)
     {
@@ -20,5 +25,29 @@ public class LevelUpPopUp : EntityLinkerController, IAnyGameLevelUpListener
     {
         _levelLabel.text = (value - 1).ToString("N0");
         _animator.SetTrigger("Appear");
+
+        StartCoroutine(LevelGlowFill(value));
+    }
+
+    private IEnumerator LevelGlowFill(int value)
+    {
+        yield return new WaitForSeconds(_playParticlesDelay);
+        var duration = _levelGlowFillParticleSystem.main.duration;
+        var t = 0f;
+        
+        _levelGlowFillParticleSystem.Stop();
+        _levelGlowFillParticleSystem.Play();
+        
+        yield return new WaitForSeconds(_fillAnimationDelay);
+
+        while (t <= duration)
+        {
+            _levelGlowFill.fillAmount = t / duration;
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        _levelGlowFill.fillAmount = 1f;
+        _levelLabel.text = value.ToString("N0");
     }
 }
